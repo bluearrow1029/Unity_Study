3e72891d64e45bf50c46b22eab09d65600fcf669
bluearrow1029 <nohyw98@gmail.com>
2020-01-31 21:26:09 +0900

    start

---
+++ Unity 2D Example/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator anime;
+    SpriteRenderer spriteRenderer;
+    public int nextMove;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anime = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Invoke("Think", 5);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+        //다음 이동방향 확인
+        Vector2 direction = new Vector2(rigid.position.x + nextMove*0.2f, rigid.position.y);
+        //디버그용 레이캐스팅
+        Debug.DrawRay(direction, Vector3.down, new Color(0, 1, 0));
+        //실제 레이캐스팅, Raycast(시작점, 방향, 길이, 충돌 레이어)
+        RaycastHit2D rayHit = Physics2D.Raycast(direction, Vector2.down, 1, LayerMask.GetMask("Platform"));
+        if(rayHit.collider == null)
+        {
+            Turn();
+        }
+    }
+
+    void Think()
+    {
+        nextMove = Random.Range(-1, 2);
+
+        anime.SetInteger("WalkSpeed", nextMove);
+
+        if (nextMove != 0)
+            spriteRenderer.flipX = nextMove == -1;
+
+        float Timer = Random.Range(2f, 5f);
+        Invoke("Think", Timer);
+    }
+
+    void Turn()
+    {
+        nextMove = nextMove * -1;
+        anime.SetInteger("WalkSpeed", nextMove);
+        spriteRenderer.flipX = nextMove == -1;
+
+        CancelInvoke();
+        Invoke("Think", 5);
+    }
+}
---
+++ Unity 2D Example/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    SpriteRenderer sprite;
+    Animator anime;
+    public float speed;
+    public float jumpPower;
+    float horizontalMove;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anime = GetComponent<Animator>();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log(col.gameObject.tag);
+        if (col.gameObject.tag == "Ground" && anime.GetBool("isJump"))
+        {
+            anime.SetBool("isJump", false);
+        }
+    }
+
+    void Update()
+    {
+        //단발 입력은 업데이트에서 쓰는 것이 좋음
+        if (Input.GetButtonDown("Jump") && !anime.GetBool("isJump"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anime.SetBool("isJump", true);
+        }
+
+        if (Input.GetButtonDown("Horizontal"))
+            sprite.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        if (horizontalMove == 0)
+            anime.SetBool("isWalk", false);
+        else
+            anime.SetBool("isWalk", true);
+    }
+
+    void FixedUpdate()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        //물리연산 이동. 가속도가 있음.
+        rigid.AddForce(Vector2.right * horizontalMove, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > speed)
+            rigid.velocity = new Vector2(speed, rigid.velocity.y);
+        else if (rigid.velocity.x < speed * -1)
+            rigid.velocity = new Vector2(speed * -1, rigid.velocity.y);
+    }
+
+}
