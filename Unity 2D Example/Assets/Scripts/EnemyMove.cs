3a7cf149cbbe777680d9d06681071570573d928d
bluearrow1029 <nohyw98@gmail.com>
2020-02-17 22:01:00 +0900

    reupload
    reupload

--- Unity 2D Example/Assets/Scripts/EnemyMove.cs
+++ Unity 2D Example/Assets/Scripts/EnemyMove.cs
@@ -6,19 +6,15 @@ public class EnemyMove : MonoBehaviour
 {
     Rigidbody2D rigid;
     Animator anime;
-    SpriteRenderer spriteRenderer;
+    SpriteRenderer sprite;
     public int nextMove;
+    
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         anime = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        Invoke("Think", 5);
-    }
-    // Update is called once per frame
-    void Update()
-    {
-
+        sprite = GetComponent<SpriteRenderer>();
+        Invoke("Move",5);
     }
 
     void FixedUpdate()
@@ -26,37 +22,45 @@ public class EnemyMove : MonoBehaviour
         rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
 
         //다음 이동방향 확인
-        Vector2 direction = new Vector2(rigid.position.x + nextMove*0.2f, rigid.position.y);
+        Vector2 direction = new Vector2(rigid.position.x + nextMove * 0.2f, rigid.position.y);
+
         //디버그용 레이캐스팅
         Debug.DrawRay(direction, Vector3.down, new Color(0, 1, 0));
+
         //실제 레이캐스팅, Raycast(시작점, 방향, 길이, 충돌 레이어)
         RaycastHit2D rayHit = Physics2D.Raycast(direction, Vector2.down, 1, LayerMask.GetMask("Platform"));
+        
         if(rayHit.collider == null)
-        {
             Turn();
-        }
     }
 
-    void Think()
+    // 이동방향 함수
+    void Move()
     {
         nextMove = Random.Range(-1, 2);
-
         anime.SetInteger("WalkSpeed", nextMove);
 
-        if (nextMove != 0)
-            spriteRenderer.flipX = nextMove == -1;
+        if (nextMove == -1)
+            sprite.flipX = true;
+        else if (nextMove == 1)
+            sprite.flipX = false;
 
-        float Timer = Random.Range(2f, 5f);
-        Invoke("Think", Timer);
+        Invoke("Move", 5);
     }
 
+    // 방향전환 함수
     void Turn()
     {
+        // 이동 방향 반대로 변경
         nextMove = nextMove * -1;
         anime.SetInteger("WalkSpeed", nextMove);
-        spriteRenderer.flipX = nextMove == -1;
+
+        if (nextMove == -1)
+            sprite.flipX = true;
+        else if (nextMove == 1)
+            sprite.flipX = false;
 
         CancelInvoke();
-        Invoke("Think", 5);
+        Invoke("Move", 5);
     }
 }
